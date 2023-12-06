eea96cfab0dba4fc74ee6a3982ffc422b2c9ddec
Supratick Dey <supratick.dey@cbnits.com>
2023-12-06 21:56:44 +0530

    paired rdd implementation

--- src/main/java/site/supratickdey/Main.java
+++ src/main/java/site/supratickdey/Main.java
@@ -3,6 +3,7 @@ package site.supratickdey;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
@@ -12,19 +13,40 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> inputData = new ArrayList<>();
-        inputData.add(35);
-        inputData.add(12);
-        inputData.add(53);
-        inputData.add(954);
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 5 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("WARN: Saturday 8 September 1942");
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("ASpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
-        JavaRDD<Tuple2<Integer, Double>> sqrtRdd = originalIntegers.map((value) -> new Tuple2<>(value, Math.sqrt(value)));
+        sc.parallelize(inputData)
+                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+                .reduceByKey((value1, value2) -> value1 + value2)
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+
+        /*
+        Verbose implementation
+        JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+
+
+
+        JavaPairRDD<String, Long> pairRDD = originalLogMessages.mapToPair(rawValue -> {
+            String[] columns = rawValue.split(":");
+            String level = columns[0];
+            // String date = columns[1];
+            return new Tuple2<>(level, 1L);
+        });
+
+        JavaPairRDD<String, Long> sumsRdd = pairRDD.reduceByKey((value1, value2) -> value1 + value2);
+        sumsRdd.foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+        */
+
 
         sc.close();
     }
