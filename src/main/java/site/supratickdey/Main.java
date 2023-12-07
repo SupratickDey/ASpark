33039dbbfeeb2ec0037d6ba3166f41a0b882c473
Supratick Dey <supratick.dey@cbnits.com>
2023-12-07 21:36:28 +0530

    implementation of flat maps

--- src/main/java/site/supratickdey/Main.java
+++ src/main/java/site/supratickdey/Main.java
@@ -7,9 +7,11 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.codehaus.janino.Java;
 import scala.Tuple2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -26,34 +28,21 @@ public class Main {
         SparkConf conf = new SparkConf().setAppName("ASpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // better approach
-        sc.parallelize(inputData)
-                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
-                .reduceByKey((value1, value2) -> value1 + value2)
-                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
-
-        // Not a good approach
-        sc.parallelize(inputData)
-                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
-                .groupByKey()
-                .foreach(tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances"));
-
         /*
         Verbose implementation
-        JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+        JavaRDD<String> sentences = sc.parallelize(inputData);
 
+        JavaRDD<String> words = sentences.flatMap(value -> Arrays.asList(value.split(" ")).iterator());
 
+        JavaRDD<String> filteredWords = words.filter(word -> word.length() > 1);
 
-        JavaPairRDD<String, Long> pairRDD = originalLogMessages.mapToPair(rawValue -> {
-            String[] columns = rawValue.split(":");
-            String level = columns[0];
-            // String date = columns[1];
-            return new Tuple2<>(level, 1L);
-        });
-
-        JavaPairRDD<String, Long> sumsRdd = pairRDD.reduceByKey((value1, value2) -> value1 + value2);
-        sumsRdd.foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+        filteredWords.foreach(value-> System.out.println(value));
         */
+
+        sc.parallelize(inputData)
+                .flatMap(value -> Arrays.asList(value.split(" ")).iterator())
+                .filter(word -> word.length() > 1)
+                .foreach(word -> System.out.println(word));
 
 
         sc.close();
