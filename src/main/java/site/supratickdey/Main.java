5b3123ea180cbd1a9ae82086e5e1de04ffe5558d
Supratick Dey <supratick.dey@cbnits.com>
2023-12-11 22:51:29 +0530

    read from local file

--- src/main/java/site/supratickdey/Main.java
+++ src/main/java/site/supratickdey/Main.java
@@ -16,17 +16,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> inputData = new ArrayList<>();
-        inputData.add("WARN: Tuesday 4 September 0405");
-        inputData.add("ERROR: Tuesday 4 September 0408");
-        inputData.add("FATAL: Wednesday 5 September 1632");
-        inputData.add("ERROR: Friday 7 September 1854");
-        inputData.add("WARN: Saturday 8 September 1942");
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("ASpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> initialRdd =  sc.textFile("src/main/resources/subtitles/input.txt");
+
+
 
         /*
         Verbose implementation
@@ -39,10 +37,10 @@ public class Main {
         filteredWords.foreach(value-> System.out.println(value));
         */
 
-        sc.parallelize(inputData)
+        initialRdd
                 .flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-                .filter(word -> word.length() > 1)
-                .foreach(word -> System.out.println(word));
+                .collect()
+                .forEach(System.out::println);
 
 
         sc.close();
